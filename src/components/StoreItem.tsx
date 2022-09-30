993b5cb956fd3f4515c7550b8eb351d909c89745
edo979 <prezentabilan@gmail.com>
2022-09-30 20:05:48 +0200

    bacis style for StoreItem comp

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,5 @@
+import { Card } from 'react-bootstrap'
+
 type StoreItemsProps = {
   id: number
   name: string
@@ -6,5 +8,20 @@ type StoreItemsProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemsProps) {
-  return <h2>store item</h2>
+  return (
+    <Card>
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: 'cover' }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  )
 }
