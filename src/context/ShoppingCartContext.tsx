33c153980f58fd97c95cca4b5e574a56f6ca529e
edo979 <prezentabilan@gmail.com>
2022-10-01 09:18:13 +0200

    finish model manipulation functions

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -28,8 +28,50 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const getItemQuantity = (id: number) =>
     cartItems.find((item) => item.id === id)?.quantity || 0
 
+  const increaseCartQuantity = (id: number) => {
+    if (cartItems.find((item) => item.id === id) == null) {
+      return setCartItems((prev) => [...prev, { id, quantity: 1 }])
+    } else {
+      setCartItems((prev) =>
+        prev.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 }
+          }
+          return item
+        })
+      )
+    }
+  }
+
+  const decreaseCartQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id)
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 }
+          }
+          return item
+        })
+      }
+    })
+  }
+
+  const removeFromCart = (id: number) =>
+    setCartItems((currentItems) =>
+      currentItems.filter((item) => item.id !== id)
+    )
+
   return (
-    <ShoppingCartContext.Provider value={{ getItemQuantity }}>
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   )
